a0201e3cb2ac8d51559a690e6b12294bff298363
suk-6 <me@suk.kr>
2024-08-07 22:39:26 +0900

    feat: Add Projects component

---
+++ src/components/projects/index.tsx
@@ -0,0 +1,10 @@
+import { DetailPage } from "../detailPage";
+
+export const Projects = () => {
+	return (
+		<DetailPage
+			title="프로젝트"
+			description="제가 진행했던 프로젝트를 소개합니다."
+		></DetailPage>
+	);
+};
