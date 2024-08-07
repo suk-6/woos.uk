f7e5be82fa5348b27ae64655d7dc6fda61e941a4
suk-6 <me@suk.kr>
2024-08-07 22:39:22 +0900

    feat: Add DetailPage component

---
+++ src/components/detailPage.tsx
@@ -0,0 +1,17 @@
+export const DetailPage = ({
+	title,
+	description,
+	children,
+}: {
+	title: string;
+	description: string;
+	children?: React.ReactNode;
+}) => (
+	<div className="relative w-full min-h-screen border-y px-10 py-12 sm:px-14 sm:py-14">
+		<div className="flex flex-col gap-1">
+			<p className="font-HSSanTokki20 text-3xl">{title}</p>
+			<p className="text-xl">{description}</p>
+		</div>
+		{children}
+	</div>
+);
