920b39e52c9845fa8510f39c8af9e362a3641211
suk-6 <me@suk.kr>
2024-08-07 21:55:00 +0900

    chore: Add Navigator component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { BusinessCard } from "@/components/businessCard";
+import { Navigator } from "@/components/navigator";
+import { useScroll } from "@/hooks/useScroll";
 
 export default function Home() {
+	const { y } = useScroll();
+	const [navVisible, setNavVisible] = useState(false);
+
+	useEffect(() => {
+		if (y > 10) setNavVisible(true);
+		else setNavVisible(false);
+	}, [y]);
+
 	return (
 		<main className=" w-screen min-h-screen font-SUITE">
+			<Navigator isVisible={navVisible} />
 			<BusinessCard />
+			<div className="h-10"></div>
 		</main>
 	);
 }
