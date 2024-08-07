72b0024e8f0487bf0c0f90689427f869885dedc4
suk-6 <me@suk.kr>
2024-08-07 21:33:20 +0900

    chore: Update font family and add keyframes for wiggle animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,19 @@ const config: Config = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		fontFamily: {
+			SUITE: ["SUITE"],
+			HSSanTokki20: ["HSSanTokki20-Regular"],
+		},
 		extend: {
-			fontFamily: {
-				HSSanTokki20: ["HSSanTokki20-Regular"],
-				SUITE: ["SUITE-Regular"],
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(-5deg)" },
+					"50%": { transform: "rotate(5deg)" },
+				},
 			},
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			animation: {
+				wiggle: "wiggle 1.5s ease-in-out infinite",
 			},
 		},
 	},
