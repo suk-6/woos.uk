42d904fb8adbc86be9a6b135d4dd3d8631541f2b
suk-6 <me@suk.kr>
2024-08-07 21:55:04 +0900

    chore: Add BusinessCard component to Home page

---
+++ src/components/businessCard.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export const BusinessCard = () => {
+	const name = "남우석";
+	const enName = "Woosuk Nam";
+
+	return (
+		<div className=" w-full min-h-screen px-10 py-12 sm:px-20 sm:py-20 flex flex-col justify-between">
+			<div className=" w-fit flex flex-col gap-2 sm:gap-4">
+				<div className="flex flex-col gap-1 sm:gap-3 text-nowrap font-HSSanTokki20 text-4xl sm:text-5xl">
+					<h1>{name.split("").join(" ")}</h1>
+					<h1>{enName}</h1>
+				</div>
+				<h2 className=" font-SUITE font-medium text-xl sm:text-2xl">
+					Backend Developer
+				</h2>
+				<div className="w-full h-1 bg-black" />
+			</div>
+			<div className=" flex flex-row justify-between items-end w-full min-w-64">
+				<div className=" flex flex-col gap-1">
+					<p>
+						<b>E.</b>{" "}
+						<Link href={"mailto:me@suk.kr"}>me@suk.kr</Link>
+					</p>
+					<p>
+						<b>T.</b> 010-9900-2512
+					</p>
+				</div>
+				<div className="animate-wiggle text-6xl">👋</div>
+			</div>
+		</div>
+	);
+};
