b63328e9c8e9b33f76d3d7987208028447603763
suk-6 <me@suk.kr>
2024-08-08 18:19:46 +0900

    refactor: Update Navigator component to use useDark hook for dark mode support

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -2,42 +2,12 @@
 
 import Link from "next/link";
 
-import { useEffect } from "react";
-
 import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 import { FaGithub, FaInstagram } from "react-icons/fa";
+import { useDark } from "@/hooks/useDark";
 
-export const Navigator = ({
-	isVisible,
-	isDark,
-	setIsDark,
-}: {
-	isVisible: boolean;
-	isDark: boolean | null;
-	setIsDark: (isDark: boolean) => void;
-}) => {
-	// https://tailwindcss.com/docs/dark-mode#supporting-system-preference-and-manual-selection
-	// TODO: Implement dark mode with system preference
-	useEffect(() => {
-		if (localStorage.getItem("isDark") === null) {
-			localStorage.setItem("isDark", "false");
-			return;
-		}
-
-		setIsDark(localStorage.getItem("isDark") === "true");
-	}, [setIsDark]);
-
-	useEffect(() => {
-		if (isDark === null) return;
-		localStorage.setItem("isDark", isDark.toString());
-
-		if (isDark) {
-			document.documentElement.classList.add("dark");
-			return;
-		} else {
-			document.documentElement.classList.remove("dark");
-		}
-	}, [isDark]);
+export const Navigator = ({ isVisible }: { isVisible: boolean }) => {
+	const { isDark, setIsDark } = useDark();
 
 	const navClass =
 		"bg-light dark:bg-dark border border-stroke border-light-nav-border dark:border-dark-nav-border p-3 rounded-lg";
