f6589ad7e2b9a17633a7d9fe7f7412a1a8804756
suk-6 <me@suk.kr>
2024-08-08 23:36:57 +0900

    refactor: Update BusinessCard component with myData from constants

--- src/components/businessCard.tsx
+++ src/components/businessCard.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Noto_Sans_Mono } from "next/font/google";
 
+import { myData } from "@/constants/data";
+
 const notoMono = Noto_Sans_Mono({ subsets: ["latin"], weight: ["400"] });
 
 const MonoB = ({ children }: { children: React.ReactNode }) => (
@@ -8,29 +10,28 @@ const MonoB = ({ children }: { children: React.ReactNode }) => (
 );
 
 export const BusinessCard = () => {
-	const name = "남우석";
-	const enName = "Woosuk Nam";
-
 	return (
 		<div className=" relative w-full min-h-[85dvh] px-10 py-12 sm:px-20 sm:py-20 flex flex-col justify-between select-none">
 			<div className=" w-fit flex flex-col gap-2 sm:gap-4">
 				<div className="flex flex-col gap-1 sm:gap-3 text-nowrap font-HSSanTokki20 text-4xl sm:text-5xl">
-					<h1>{name.split("").join(" ")}</h1>
-					<h1>{enName}</h1>
+					<h1>{myData.name.split("").join(" ")}</h1>
+					<h1>{myData.enName}</h1>
 				</div>
 				<h2 className=" font-SUITE font-medium text-xl sm:text-2xl">
-					Backend Developer
+					{myData.job}
 				</h2>
 				<div className="w-1/3 h-1 bg-light-contrast dark:bg-dark-contrast" />
 			</div>
 			<div className=" flex flex-row justify-between items-end w-full min-w-60">
 				<div className=" grid grid-cols-[0.2fr_1fr] gap-y-1">
 					<MonoB>E.</MonoB>
-					<Link href={"mailto:me@suk.kr"}>me@suk.kr</Link>
+					<Link href={`mailto:${myData.email}`}>{myData.email}</Link>
 					<MonoB>W.</MonoB>
-					<Link href={"https://suk.kr"}>https://suk.kr</Link>
+					<Link href={myData.website}>{myData.website}</Link>
 					<MonoB>T.</MonoB>
-					<Link href={"tel:01099002512"}>010-9900-2512</Link>
+					<Link href={`tel:${myData.phone.replaceAll("-", "")}`}>
+						{myData.phone}
+					</Link>
 				</div>
 				<div className="animate-wiggle text-6xl">👋</div>
 			</div>
