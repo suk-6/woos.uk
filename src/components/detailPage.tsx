e3af21cfa5e2b2cb6e164966bf8cbe45ed189e34
suk-6 <me@suk.kr>
2024-08-08 16:42:16 +0900

    refactor: Update detailPage component to include full viewport height and border color for light and dark mode

--- src/components/detailPage.tsx
+++ src/components/detailPage.tsx
@@ -7,7 +7,7 @@ export const DetailPage = ({
 	description: string;
 	children?: React.ReactNode;
 }) => (
-	<div className="relative w-full min-h-screen border-y border-light-pageBorder dark:border-dark-pageBorder px-10 py-12 sm:px-14 sm:py-14">
+	<div className="relative w-full min-h-[100dvh] border-y border-light-pageBorder dark:border-dark-pageBorder px-10 py-12 sm:px-14 sm:py-14">
 		<div className="flex flex-col gap-1">
 			<p className="font-HSSanTokki20 text-3xl">{title}</p>
 			<p className="text-xl">{description}</p>
