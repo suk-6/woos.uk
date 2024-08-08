0970eadbe3480253e6db10430c4dc25d739c9193
suk-6 <me@suk.kr>
2024-08-08 21:26:16 +0900

    refactor: Update Tile component to use lazy loading for images

--- src/components/gallery/tile.tsx
+++ src/components/gallery/tile.tsx
@@ -8,21 +8,21 @@ type TileProps = {
 
 export const Tile = ({ title, description, bg }: TileProps) => {
 	return (
-		<div className=" relative snap-always snap-center w-full h-full p-2 z-20">
-			<div className=" relative w-full h-full bg-gray-200 rounded-xl shadow-md">
-				{bg && (
-					<div className="absolute bottom-0 left-0 right-0 top-0 w-full h-5/6">
+		<div className=" relative snap-always snap-center min-w-[35rem] h-full p-2">
+			<div className=" relative w-full h-full flex flex-col bg-gray-200 rounded-xl shadow-md z-10s">
+				<div className=" relative w-full h-full z-30 py-1">
+					{bg && (
 						<Image
 							src={bg}
 							alt={title}
-							layout="fixed"
-							className="w-full h-full object-cover rounded-t-xl"
+							layout="fill"
+							className="w-full h-full object-cover rounded-t-xl z-30"
 						/>
-					</div>
-				)}
-				<div className="w-full h-full flex flex-col justify-end mx-4 pb-3 text-gray-700">
-					<h2 className="text-md font-semibold">{title}</h2>
-					<p className="text-sm text-nowrap">{description}</p>
+					)}
+				</div>
+				<div className=" h-fit px-3 py-3 flex flex-col items-start justify-end">
+					<h2 className="text-md font-semibold z-30">{title}</h2>
+					<p className="text-sm text-nowrap z-30">{description}</p>
 				</div>
 			</div>
 		</div>
