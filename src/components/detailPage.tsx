82a06d8ee826388673a1624f4ec71a57ca810c70
suk-6 <me@suk.kr>
2024-08-08 21:40:47 +0900

    refactor: Update detailPage component layout for better readability

--- src/components/detailPage.tsx
+++ src/components/detailPage.tsx
@@ -7,8 +7,8 @@ export const DetailPage = ({
 	description: string;
 	children?: React.ReactNode;
 }) => (
-	<div className="relative w-full min-h-[100dvh] border-y border-light-pageBorder dark:border-dark-pageBorder px-10 py-12 sm:px-14 sm:py-14">
-		<div className="flex flex-col gap-1">
+	<div className="relative w-full min-h-[100dvh] border-y border-light-pageBorder dark:border-dark-pageBorder py-12 sm:py-14">
+		<div className="flex flex-col gap-1 px-10 sm:px-14">
 			<p className="font-HSSanTokki20 text-3xl">{title}</p>
 			<p className="text-xl">{description}</p>
 		</div>
