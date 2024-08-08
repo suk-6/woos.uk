73677d2eeaa24d3a80b81b1afde7bc6119344c52
suk-6 <me@suk.kr>
2024-08-08 23:22:44 +0900

    refactor: Update Projects page to improve project descriptions

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -5,10 +5,7 @@ import { myData } from "@/data";
 
 export const Projects = () => {
 	return (
-		<DetailPage
-			title="프로젝트"
-			description="제가 진행했던 프로젝트를 소개합니다."
-		>
+		<DetailPage title="프로젝트" description="내 프로젝트를 소개합니다.">
 			{myData.projects.map((project) => (
 				<Project
 					key={project.title}
