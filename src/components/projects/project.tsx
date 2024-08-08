4ba638afbd2da4b03d9cb88a01cccca6d53d14f0
suk-6 <me@suk.kr>
2024-08-08 23:22:56 +0900

    refactor: Update Project component to display project details and links

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -11,7 +11,7 @@ type ProjectProps = {
 
 export const Project = (props: ProjectProps) => {
 	return (
-		<div className=" max-w-[60rem] h-full px-10 sm:px-14 pt-10 lg:pt-16 flex flex-col gap-0">
+		<div className=" max-w-[60rem] h-full px-10 sm:px-14 flex flex-col gap-0">
 			<div className=" flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-end sm:gap-0">
 				<div>
 					<h1 className=" text-lg sm:text-2xl font-semibold">
