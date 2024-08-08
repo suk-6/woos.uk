29db34f36fc926c143509c88ebdb068e5f6648ac
suk-6 <me@suk.kr>
2024-08-09 00:19:19 +0900

    feat: Add SocialCard component to Home page

---
+++ src/components/socialCard.tsx
@@ -0,0 +1,5 @@
+export const SocialCard = () => {
+	return (
+		<div className=" relative w-full min-h-[85dvh] px-10 py-12 sm:px-20 sm:py-20 flex flex-col justify-between select-none border-y border-light-pageBorder dark:border-dark-pageBorder"></div>
+	);
+};
