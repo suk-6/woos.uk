45b7e964e9d2f61b476f2a9798044d3b58151f06
suk-6 <me@suk.kr>
2024-08-08 20:46:59 +0900

    refactor: Add gallery with image tiles to Projects page

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,10 +1,42 @@
 import { DetailPage } from "../detailPage";
+import { Gallery } from "../gallery/gallery";
+import { Tile } from "../gallery/tile";
+
+import dytimetalbe from "@/../public/dytimetable.png";
 
 export const Projects = () => {
 	return (
 		<DetailPage
 			title="프로젝트"
 			description="제가 진행했던 프로젝트를 소개합니다."
-		></DetailPage>
+		>
+			<Gallery>
+				<Tile
+					title="덕영시간표"
+					description="매 시간마다, 다음 교시 과목을 알려주는 서비스"
+					bg={dytimetalbe}
+				/>
+				<Tile
+					title="덕영시간표"
+					description="매 시간마다, 다음 교시 과목을 알려주는 서비스"
+					bg={dytimetalbe}
+				/>
+				<Tile
+					title="덕영시간표"
+					description="매 시간마다, 다음 교시 과목을 알려주는 서비스"
+					bg={dytimetalbe}
+				/>
+				<Tile
+					title="덕영시간표"
+					description="매 시간마다, 다음 교시 과목을 알려주는 서비스"
+					bg={dytimetalbe}
+				/>
+				<Tile
+					title="덕영시간표"
+					description="매 시간마다, 다음 교시 과목을 알려주는 서비스"
+					bg={dytimetalbe}
+				/>
+			</Gallery>
+		</DetailPage>
 	);
 };
