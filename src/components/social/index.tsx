caa62ef96fa1eeb98e142d0e332d476b76f0ddf7
suk-6 <me@suk.kr>
2024-08-09 21:15:33 +0900

    refactor: support mobile

--- src/components/social/index.tsx
+++ src/components/social/index.tsx
@@ -5,7 +5,7 @@ import { myData } from "@/constants/data";
 export const SocialCard = () => {
 	return (
 		<div className=" relative w-full min-h-fit px-10 py-12 sm:px-20 sm:py-20 flex flex-col justify-between select-none border-y border-light-pageBorder dark:border-dark-pageBorder">
-			<div className=" flex flex-row flex-wrap gap-6">
+			<div className=" flex flex-row flex-wrap justify-center gap-8 sm:gap-12">
 				{myData.socials.map((social, index) => (
 					<SocialBlock
 						key={index}
