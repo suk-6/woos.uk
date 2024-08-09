9c15576ef2b2a2dcc4b9496e1b99f4ec37f0c6c3
suk-6 <me@suk.kr>
2024-08-09 17:17:14 +0900

    refactor: Add SocialIcon constant for rendering social media icons

---
+++ src/models/social.tsx
@@ -0,0 +1,15 @@
+import {
+	FaGithub,
+	FaGlobe,
+	FaInstagram,
+	FaLinkedin,
+	FaTwitter,
+} from "react-icons/fa";
+
+export const SocialIcon = {
+	Instagram: <FaInstagram className=" w-fit h-full" />,
+	Twitter: <FaTwitter className=" w-fit h-full" />,
+	Github: <FaGithub className=" w-fit h-full" />,
+	Linkedin: <FaLinkedin className=" w-fit h-full" />,
+	Website: <FaGlobe className=" w-fit h-full" />,
+};
