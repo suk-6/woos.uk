0d01fb64cc8fb4f0203a4c402fd5218f943b17e9
suk-6 <me@suk.kr>
2024-08-11 16:13:30 +0900

    refactor: Update project page layout to make it responsive

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -4,7 +4,7 @@ import { ProjectType } from "@/models/data";
 
 export const Project = ({ data }: { data: ProjectType }) => {
 	return (
-		<div className=" w-[60rem] h-full px-10 sm:px-14 flex flex-col gap-0">
+		<div className=" w-full sm:max-w-[60rem] h-full px-10 sm:px-14 flex flex-col gap-0">
 			<div className=" flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-end sm:gap-0">
 				<div>
 					<h1 className=" text-xl sm:text-2xl font-semibold">
