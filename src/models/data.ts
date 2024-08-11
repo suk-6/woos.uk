5c04a9eaf7d0c3bc69c0cbf2df020b9ee28de3a6
suk-6 <me@suk.kr>
2024-08-11 15:55:53 +0900

    feat: Add date information to ProjectType interface

--- src/models/data.ts
+++ src/models/data.ts
@@ -15,6 +15,17 @@ export type ProjectType = {
 	title: string;
 	description: string;
 	techStack?: string[];
+	date: {
+		start: {
+			year: number;
+			month: number;
+		};
+		isEnd: boolean;
+		end?: {
+			year: number;
+			month: number;
+		};
+	};
 	links?: {
 		website?: string;
 		github?: string;
