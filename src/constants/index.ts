4f24a604ed3aee56b7bf5070a6f7eff80331355a
suk-6 <me@suk.kr>
2024-08-12 20:47:21 +0900

    feat: Seperate objects

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+import { DataType } from "@/models/data";
+
+import { projects } from "./projects";
+import { socials } from "./socials";
+
+export const myData: DataType = {
+	name: "남우석",
+	enName: "Woosuk Nam",
+	job: "Backend Developer",
+	email: "me@suk.kr",
+	website: "https://suk.kr",
+	phone: "010-9900-2512",
+	socials: socials,
+	projects: projects,
+};
---
+++ src/constants/socials.ts
@@ -0,0 +1,29 @@
+import { SocialType } from "@/models/data";
+
+export const socials: SocialType[] = [
+	{
+		name: "Blog",
+		handle: "Suk's Blog",
+		link: "https://blog.suk.kr",
+	},
+	{
+		name: "Github",
+		handle: "@suk-6",
+		link: "https://github.com/suk-6",
+	},
+	{
+		name: "Linkedin",
+		handle: "@woosuknam",
+		link: "https://www.linkedin.com/in/woosuknam",
+	},
+	{
+		name: "Twitter",
+		handle: "@woosuknam",
+		link: "https://x.com/woosuknam",
+	},
+	{
+		name: "Instagram",
+		handle: "@woosuk_",
+		link: "https://instagram.com/woosuk_",
+	},
+];
