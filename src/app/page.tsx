a8c8e5b6adccb359980700ab0798f532608456a2
suk-6 <me@suk.kr>
2024-08-13 00:30:31 +0900

    refactor: Optimize scroll functionality in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,36 +8,31 @@ import { Projects } from "@/components/projects";
 import { SocialCard } from "@/components/social";
 import { Awards } from "@/components/awards";
 import { Activities } from "@/components/activities";
+import { useGetCardHeights } from "@/hooks/useGetCardHeights";
 
 export default function Home() {
 	const [container, setContainer] = useState<HTMLDivElement | null>(null);
 	const [navVisible, setNavVisible] = useState(false);
+	const { cardHeights } = useGetCardHeights(container);
 
 	useEffect(() => {
 		if (!container) return;
-
-		const cards = container.querySelectorAll(".scroll-items");
-		const cardHeights: number[] = [];
-
-		cards.forEach((c) => {
-			const card = c as HTMLDivElement;
-			cardHeights.push(card.offsetTop);
-		});
 
 		const f = () => {
 			if (container.scrollTop > 100) setNavVisible(true);
 			else setNavVisible(false);
 
-			if (navigator.userAgent.includes("AppleWebKit")) {
+			if (navigator.userAgent.includes("iPhone")) {
 				if (container.scrollTop > cardHeights[2])
 					container.classList.remove("snap-y");
 				else container.classList.add("snap-y");
 			}
 		};
+		f();
 
 		container.addEventListener("scroll", f);
 		return () => container.removeEventListener("scroll", f);
-	}, [container]);
+	}, [cardHeights, container]);
 
 	return (
 		<main className=" fixed w-full h-full font-SUITE overflow-hidden">
