b1c6db4902509fef2ee3b557ccdef6bb95d8a1db
suk-6 <me@suk.kr>
2024-08-13 16:00:54 +0900

    feat: show award data

--- src/components/awards/index.tsx
+++ src/components/awards/index.tsx
@@ -1,7 +1,20 @@
+import { personalData } from "@/constants";
 import { DetailPage } from "../detailPage";
+import { Award } from "./award";
 
 export const Awards = () => {
+	const sortedAwards = personalData.awards.sort((a, b) => {
+		const aDate = new Date(a.date.year, a.date.month);
+		const bDate = new Date(b.date.year, b.date.month);
+
+		return bDate.getTime() - aDate.getTime();
+	});
+
 	return (
-		<DetailPage title="수상" description="여기서 상 받았어요."></DetailPage>
+		<DetailPage title="수상" description="여기서 상 받았어요.">
+			{sortedAwards.map((award, i) => (
+				<Award key={i} data={award} />
+			))}
+		</DetailPage>
 	);
 };
