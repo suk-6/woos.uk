f647c8f07316bfab0c3779db92d91bf2bc6e5bbc
suk-6 <me@suk.kr>
2024-08-13 14:36:22 +0900

    refactor: Sort projects by start date in Projects component

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -4,9 +4,16 @@ import { Project } from "./project";
 import { personalData } from "@/constants";
 
 export const Projects = () => {
+	const sortedProjects = personalData.projects.sort((a, b) => {
+		const aDate = new Date(a.date.start.year, a.date.start.month);
+		const bDate = new Date(b.date.start.year, b.date.start.month);
+
+		return bDate.getTime() - aDate.getTime();
+	});
+
 	return (
 		<DetailPage title="프로젝트" description="내 프로젝트를 소개합니다.">
-			{personalData.projects.map((project) => (
+			{sortedProjects.map((project) => (
 				<Project key={project.title} data={project} />
 			))}
 		</DetailPage>
