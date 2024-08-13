49cb8f73a5b5905c54e1c3270b2f152a756cac15
suk-6 <me@suk.kr>
2024-08-14 00:47:38 +0900

    feat: add naver-site-verification to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ export const metadata: Metadata = {
 		locale: "ko_KR",
 		siteName: "@woosuknam",
 	},
+	verification: {
+		other: {
+			"naver-site-verification":
+				"6cdbf3525bf38b1c16e33e8b378163eaec6dc6a6",
+		},
+	},
 };
 
 export default function RootLayout({
