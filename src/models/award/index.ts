b08b2e759a8f04bc8d0169cb8a20b0a6ad5a7ac4
suk-6 <me@suk.kr>
2024-08-13 16:00:03 +0900

    refactor(models): Update AwardType in data model

--- src/models/award/index.ts
+++ src/models/award/index.ts
@@ -1,8 +1,16 @@
 export type AwardType = {
-	title: string;
+	competition: string;
+	prize: PrizeType;
+	host: string;
+	links?: {
+		website?: string;
+		github?: string;
+	};
 	date: {
 		year: number;
 		month: number;
 	};
-	content: string;
+	content: string | null;
 };
+
+type PrizeType = "대상" | "최우수상" | "우수상" | "장려상" | string | null;
