90b8dadf35d6e2622e382c4b089f94c4aaa0600c
suk-6 <me@suk.kr>
2024-08-15 11:14:35 +0900

    refactor: Update date display

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -7,6 +7,17 @@ const monthFormat = (month: number) =>
 	month < 10 ? `0${month}` : month.toString();
 
 export const Project = ({ data }: { data: ProjectType }) => {
+	let isSameDate = false;
+
+	if (data.date.isEnd) {
+		if (
+			data.date.start.year === data.date.end?.year &&
+			data.date.start.month === data.date.end?.month
+		) {
+			isSameDate = true;
+		}
+	}
+
 	return (
 		<div className=" w-full sm:max-w-[60rem] h-full px-10 sm:px-14 flex flex-col gap-0">
 			<div className=" flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-end sm:gap-0">
@@ -19,25 +30,32 @@ export const Project = ({ data }: { data: ProjectType }) => {
 					</h2>
 				</div>
 				<div className=" flex flex-row sm:flex-col-reverse justify-between sm:justify-normal sm:items-end sm:gap-1">
-					<div className=" flex flex-row gap-1 text-sm font-semibold">
-						<div>
+					{isSameDate ? (
+						<div className="text-sm font-semibold">
 							{data.date.start.year}.
 							{monthFormat(data.date.start.month)}
 						</div>
-						<div>~</div>
-						<div
-							className={`text-nowrap ${
-								!data.date.isEnd &&
-								"text-primary dark:text-secondary"
-							}`}
-						>
-							{data.date.isEnd
-								? `${data.date.end?.year}.${monthFormat(
-										data.date.end?.month!
-								  )}`
-								: "진행 중"}
+					) : (
+						<div className=" flex flex-row gap-1 text-sm font-semibold">
+							<div>
+								{data.date.start.year}.
+								{monthFormat(data.date.start.month)}
+							</div>
+							<div>~</div>
+							<div
+								className={`text-nowrap ${
+									!data.date.isEnd &&
+									"text-primary dark:text-secondary"
+								}`}
+							>
+								{data.date.isEnd
+									? `${data.date.end?.year}.${monthFormat(
+											data.date.end?.month!
+									  )}`
+									: "진행 중"}
+							</div>
 						</div>
-					</div>
+					)}
 					{data.links && (
 						<div className="pb-1 flex flex-row gap-2">
 							{data.links.website && (
