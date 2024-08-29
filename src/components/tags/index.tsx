2a0afe391d7cae587e30e1918c45c6a81bbd5c97
suk-6 <me@suk.kr>
2024-08-29 20:47:57 +0900

    feat: Add Tags component for displaying tags

---
+++ src/components/tags/index.tsx
@@ -0,0 +1,15 @@
+import { Tag } from "./tag";
+
+export const Tags = ({ tags }: { tags?: string[] }) => {
+	return (
+		<div>
+			{tags && (
+				<div className=" w-full flex flex-row flex-wrap gap-1 pt-2 pb-3">
+					{tags.map((tag, index) => (
+						<Tag key={index} content={tag} />
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
