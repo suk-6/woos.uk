2053fb9359a551d004624f03e565cca6ee999cc8
suk-6 <me@suk.kr>
2024-10-31 18:33:23 +0900

    refactor(style): adjust gap spacing in tags component

--- src/components/tags/index.tsx
+++ src/components/tags/index.tsx
@@ -4,7 +4,7 @@ export const Tags = ({ tags }: { tags?: string[] }) => {
 	return (
 		<div>
 			{tags && (
-				<div className=" w-full flex flex-row flex-wrap gap-x-1 pb-3">
+				<div className=" w-full flex flex-row flex-wrap gap-1 pb-3">
 					{tags.map((tag, index) => (
 						<Tag key={index} content={tag} />
 					))}
