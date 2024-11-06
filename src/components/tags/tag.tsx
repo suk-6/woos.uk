7a38973a39f1920ba5403c6c062c1870aa208707
suk-6 <me@suk.kr>
2024-11-06 21:21:11 +0900

    refactor(style): change tag bg-color

--- src/components/tags/tag.tsx
+++ src/components/tags/tag.tsx
@@ -1,5 +1,5 @@
 export const Tag = ({ content }: { content: string }) => (
-	<div className=" bg-primary dark:bg-secondary text-light-sibao-1 dark:text-light-sibao-9 text-xs font-normal px-2 py-[0.2rem] rounded-full">
+	<div className=" bg-primary dark:brightness-[0.9] text-light-sibao-1 text-xs font-normal px-2 py-[0.2rem] rounded-full">
 		{content}
 	</div>
 );
